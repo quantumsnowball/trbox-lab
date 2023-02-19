e2a0e9b854528be31ca386d8df9c21c90a7f3de4
Quantum Snowball <quantum.snowball@gmail.com>
2023-02-19 14:06:51 +0800

    cleanup:

--- components/index/BreadCrumb.tsx
+++ components/index/BreadCrumb.tsx
@@ -1,9 +1,6 @@
 import HomeIcon from '@mui/icons-material/Home';
-import WhatshotIcon from '@mui/icons-material/Whatshot';
-import GrainIcon from '@mui/icons-material/Grain';
-import NavigateNextIcon from '@mui/icons-material/NavigateNext';
-import { FC, useState } from 'react';
-import { Breadcrumbs, Menu, MenuItem, Typography } from '@mui/material';
+import { FC, } from 'react';
+import { Breadcrumbs, Typography } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
 import { layoutTempActions } from '@/redux/slices/layoutTemp';
@@ -33,12 +30,6 @@ const BreadCrumbs = () => {
     useSelector((s: RootState) => s.layoutTemp.breadCrumbs.nodes),
     (i: number) => () => dispatch(layoutTempActions.shortenNodes(i))
   ]
-  const [anchor, setAnchor] = useState<null | HTMLElement>(null)
-  const [visible, setVisible] = useState(false)
-  const onClickBreadCrumb = (event: React.MouseEvent<HTMLElement>) => {
-    setVisible(prev => !prev)
-    setAnchor(event.currentTarget)
-  }
 
   const isLastDir = () => {
     const last = nodes.at(-1)
