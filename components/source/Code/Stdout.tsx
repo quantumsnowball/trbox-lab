a9e883fb2c771896fc10c1df415fb0a069ec7bd3
Quantum Snowball <quantum.snowball@gmail.com>
2023-02-22 13:55:24 +0800

    layout: terminal stdout

--- components/source/Code/Stdout.tsx
+++ components/source/Code/Stdout.tsx
@@ -1,29 +1,34 @@
 import { RunResult } from "@/redux/slices/apiSlice"
-import { Paper, Typography } from "@mui/material"
+import { Box } from "@mui/material"
 import { FC } from "react"
+import SyntaxHighlighter from "react-syntax-highlighter"
+// use cjs module instead of esm when using NextJS, important!
+// good looking themes you may consider: 
+//   gruvboxDark, srcery, railscasts
+//   ref: https://react-syntax-highlighter.github.io/react-syntax-highlighter/demo/
+import { railscasts as colorScheme } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
 
 const Stdout: FC<{ runResult: RunResult | undefined }> = ({ runResult }) => {
   return (
-    <>
-      {runResult ?
-        <>
-          <Typography>Stdout:</Typography>
-          <Paper
-            sx={{ m: 1, p: 1 }}
-          >
-            <Typography
-              sx={{ whiteSpace: 'pre-line' }}
-            >
-              <code>
-                {runResult.stdout}
-              </code>
-            </Typography>
-          </Paper>
-        </>
-        : null
-      }
-    </>
-
+    <Box
+      id='viewer-div'
+      sx={{
+        display: 'flex',
+        flexFlow: 'column nowrap',
+        height: '100%',
+        overflow: 'auto',
+        mx: 1
+      }}
+    >
+      <SyntaxHighlighter
+        language='powershell'
+        style={colorScheme}
+        showLineNumbers={false}
+        customStyle={{ fontSize: '1.0em', height: '100%' }}
+      >
+        {runResult?.stdout ?? ''}
+      </SyntaxHighlighter>
+    </Box >
   )
 }
 
