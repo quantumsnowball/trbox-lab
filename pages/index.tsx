2846f7f2785986a141a0998d40357ca63941d3f8
Quantum Snowball <quantum.snowball@gmail.com>
2023-02-22 18:38:23 +0800

    rft: index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,16 @@
-import { Paper, styled, Typography } from '@mui/material'
+import { Paper, Typography } from '@mui/material'
 
-
-const ContentDiv = styled('div')`
-  /* take all vertical space */
-  flex: 1 1 auto;
-  /* single item each row */
-  display: flex;
-  flex-flow: column;
-  /* align vertically */
-  justify-content: center;
-  /* align horizontally */
-  align-items: center;
-`;
 
 export default function Home() {
   return (
-    <ContentDiv>
+    <div
+      id='content-div'
+      className='full flex-column expanding'
+    >
       <Paper
-        sx={{
-          display: 'flex',
-          flexFlow: 'column nowrap',
-          height: '100%',
-          width: '100%',
-          overflow: 'auto',
-          justifyContent: 'Center',
-        }}>
+        className='full flex-column expanding'
+        sx={{ justifyContent: 'center' }}
+      >
         <Typography
           variant='h4'
           sx={{ textAlign: 'center' }}
@@ -32,6 +18,6 @@ export default function Home() {
           Welcom to TrBox Lab
         </Typography>
       </Paper>
-    </ContentDiv>
+    </div>
   )
 }
