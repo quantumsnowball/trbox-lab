8c15c0917c42aa12ca7efcf49c0dc1cfb135195a
Quantum Snowball <quantum.snowball@gmail.com>
2023-02-25 22:16:40 +0800

    format: cleaner

--- common/utils.ts
+++ common/utils.ts
@@ -5,7 +5,7 @@ export const cleanUrl = (url: string) => url.replace(/([^:]\/)\/+/g, "$1")
 export const resultDirDatetimeFormatted = (name: string) =>
   name
     .replace(`${RESULT_DIR_PREFIX}_`, '')
-    .replace('T', ' @ ')
+    .replace('T', ' - ')
     .replaceAll('.', ':')
     .slice(0, -7)
 
