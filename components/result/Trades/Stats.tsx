885e99accbdf05f41a05807f47291934a63f730b
Quantum Snowball <quantum.snowball@gmail.com>
2023-03-07 14:07:35 +0800

    layout: Section, scroll

--- components/result/Trades/Stats.tsx
+++ components/result/Trades/Stats.tsx
@@ -2,18 +2,33 @@ import {
   Accordion,
   AccordionDetails,
   AccordionSummary,
+  Paper,
   TextField,
   Typography,
   useMediaQuery,
   useTheme
 } from "@mui/material";
-import { FC, useState } from "react";
+import { FC, PropsWithChildren, useState } from "react";
 import ExpandLessIcon from '@mui/icons-material/ExpandLess'
 import { Box } from "@mui/system";
 
-const Column = () => {
+const Section: FC<{ title: string } & PropsWithChildren> = ({ title, children }) => {
   return (
     <>
+      <Typography
+        sx={{
+          textAlign: 'center',
+          my: 0.5,
+        }}
+      >
+        {title}
+      </Typography>
+      <Box
+        className='flex row start'
+        sx={{ flexWrap: 'wrap' }}
+      >
+        {children}
+      </Box>
     </>
   )
 }
@@ -38,6 +53,8 @@ const Field: FC<{ name: string, value: string }> = ({ name, value }) => {
 
 const Stats = () => {
   const [expanded, setExpanded] = useState(true)
+  const theme = useTheme()
+  const isSmall = useMediaQuery(theme.breakpoints.down('sm'))
 
   return (
     <Accordion
@@ -48,23 +65,42 @@ const Stats = () => {
       <AccordionSummary
         expandIcon={<ExpandLessIcon />}
       >
-        <Typography>
+        <Typography
+          variant='h6'
+        >
           Statistics
         </Typography>
       </AccordionSummary>
-      <AccordionDetails>
-        <Box
-          className='flex row start'
-          sx={{ flexWrap: 'wrap' }}
-        >
-          <Field name='Trade count' value='20' />
+      <AccordionDetails
+        className='scroll'
+        sx={{
+          maxHeight: isSmall ? '40vh' : '35vh'
+        }}
+      >
+        <Section title='Buys'>
+          <Field name='Total count' value='20' />
           <Field name='Average trade interval' value='3.02 days' />
           <Field name='Average trade quantity' value='$200.3454' />
           <Field name='Average trade value' value='200.3454 USDT' />
+          <Field name='Average trade fees' value='1.3454 USDT' />
+        </Section>
+        <Section title='Sells'>
+          <Field name='Total count' value='20' />
+          <Field name='Average trade interval' value='3.02 days' />
+          <Field name='Average trade quantity' value='$200.3454' />
+          <Field name='Average trade value' value='200.3454 USDT' />
+          <Field name='Average trade fees' value='1.3454 USDT' />
+        </Section>
+        <Section title='Win'>
           <Field name='Win rate' value='65%' />
+          <Field name='Average duration' value='1.5 days' />
           <Field name='Average win %' value='+12.05%' />
+        </Section>
+        <Section title='Loss'>
+          <Field name='Loss rate' value='45%' />
+          <Field name='Average duration' value='2.5 days' />
           <Field name='Average loss %' value='-5.05%' />
-        </Box>
+        </Section>
       </AccordionDetails>
     </Accordion>
   )
