a2e443332c1c982c923d1b8067034036189c924b
Quantum Snowball <quantum.snowball@gmail.com>
2023-03-07 12:48:09 +0800

    rft: TradeList

---
+++ components/result/Trades/TradeList.tsx
@@ -0,0 +1,81 @@
+import { roundCurrency, roundFloat } from "@/common/utils";
+import { useGetTradesQuery } from "@/redux/slices/apiSlice";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { FC } from "react";
+
+
+const formatColumn = (column: string, val: string | number) => {
+  switch (column) {
+    case 'Date':
+      return (val as string).replace('T', ' ').slice(0, -4)
+    case 'Quantity':
+      return roundFloat(4)(val as number)
+    case 'Price':
+    case 'GrossProceeds':
+    case 'Fees':
+    case 'NetProceeds':
+      return roundCurrency(2)(val as number)
+    default:
+      return val
+  }
+}
+
+const TradeList: FC<{ path: string, strategy: string }> = ({ path, strategy }) => {
+  const { data } = useGetTradesQuery({ path, strategy })
+  const fields = data?.schema.fields.map((field: { name: string }) => field.name)
+  const trades = data?.data
+  return (
+    <TableContainer
+      className='expanding'
+      component={Paper}
+    >
+      <Table
+        stickyHeader
+        size='small'
+      >
+        <TableHead>
+          <TableRow>
+            {fields?.map(colname =>
+              <TableCell
+                key={colname}
+                align='right'
+              >
+                {colname}
+              </TableCell>)}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {
+            trades?.map((trade, i) =>
+              <TableRow
+                key={i}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                {fields?.map(name =>
+                  <TableCell
+                    key={name}
+                    className='nowrap'
+                    align='right'
+                  >
+                    {formatColumn(name, trade[name])}
+                  </TableCell>
+
+                )}
+              </TableRow>
+            )
+          }
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
+
+export default TradeList
--- components/result/Trades/index.tsx
+++ components/result/Trades/index.tsx
@@ -1,87 +1,14 @@
 import { FileNode } from "@/common/types";
-import { roundCurrency, roundFloat } from "@/common/utils";
-import { useGetMetaQuery, useGetTradesQuery } from "@/redux/slices/apiSlice";
+import { useGetMetaQuery } from "@/redux/slices/apiSlice";
 import {
   Box,
-  Paper,
   Tab,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
   Tabs,
 } from "@mui/material";
 import { FC, useState } from "react";
 import Stats from "./Stats";
+import TradeList from "./TradeList";
 
-
-const formatColumn = (column: string, val: string | number) => {
-  switch (column) {
-    case 'Date':
-      return (val as string).replace('T', ' ').slice(0, -4)
-    case 'Quantity':
-      return roundFloat(4)(val as number)
-    case 'Price':
-    case 'GrossProceeds':
-    case 'Fees':
-    case 'NetProceeds':
-      return roundCurrency(2)(val as number)
-    default:
-      return val
-  }
-}
-
-const Content: FC<{ path: string, strategy: string }> = ({ path, strategy }) => {
-  const { data } = useGetTradesQuery({ path, strategy })
-  const fields = data?.schema.fields.map((field: { name: string }) => field.name)
-  const trades = data?.data
-  return (
-    <TableContainer
-      className='expanding'
-      component={Paper}
-    >
-      <Table
-        stickyHeader
-        size='small'
-      >
-        <TableHead>
-          <TableRow>
-            {fields?.map(colname =>
-              <TableCell
-                key={colname}
-                align='right'
-              >
-                {colname}
-              </TableCell>)}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {
-            trades?.map((trade, i) =>
-              <TableRow
-                key={i}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                {fields?.map(name =>
-                  <TableCell
-                    key={name}
-                    className='nowrap'
-                    align='right'
-                  >
-                    {formatColumn(name, trade[name])}
-                  </TableCell>
-
-                )}
-              </TableRow>
-            )
-          }
-        </TableBody>
-      </Table>
-    </TableContainer>
-  )
-}
 
 const Tabbed: FC<{ path: string | undefined }> = ({ path }) => {
   const { data: meta } = useGetMetaQuery(path ?? '')
@@ -109,7 +36,7 @@ const Tabbed: FC<{ path: string | undefined }> = ({ path }) => {
       {
         path && strategies &&
         <>
-          <Content path={path} strategy={strategies[tabId]} />
+          <TradeList path={path} strategy={strategies[tabId]} />
           <Stats />
         </>
       }
