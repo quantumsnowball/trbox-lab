71cff13d0e2c93d7fbd4df5170dc4fcf2b3a80b5
Quantum Snowball <quantum.snowball@gmail.com>
2023-03-11 12:48:01 +0800

    layout: visibility of control buttons

--- components/result/Study/Marks.tsx
+++ components/result/Study/Marks.tsx
@@ -1,20 +1,43 @@
-import { Accordion, AccordionDetails, AccordionSummary, Box, Typography, Checkbox } from "@mui/material"
+import { Accordion, AccordionDetails, AccordionSummary, Box, Typography, Checkbox, ToggleButtonGroup, ToggleButton } from "@mui/material"
 import { FC, useState } from "react"
 import ExpandLessIcon from '@mui/icons-material/ExpandLess'
 import { useGetMarksIndexQuery } from "@/redux/slices/apiSlice"
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
 
 
 const Row: FC<{ name: string }> = ({ name }) => {
+  const [visible, setVisible] = useState(false)
   return (
     <Box
       className='flex row spread'
+      sx={{ minHeight: 48 }}
     >
-      <Typography>
-        {name}
-      </Typography>
-      <Checkbox
-        color='success'
-      />
+      <Box
+        className='flex row'
+      >
+        <Checkbox
+          color='success'
+          icon={<VisibilityOffOutlinedIcon />}
+          checkedIcon={<VisibilityOutlinedIcon />}
+          sx={{ mr: 1 }}
+          checked={visible}
+          onClick={() => setVisible(p => !p)}
+        />
+        <Typography>
+          {name}
+        </Typography>
+      </Box>
+      {visible &&
+        <ToggleButtonGroup size='small'>
+          <ToggleButton value='main'>
+            Main
+          </ToggleButton>
+          <ToggleButton value='overlay'>
+            Overlay
+          </ToggleButton>
+        </ToggleButtonGroup>
+      }
     </Box>
   )
 }
