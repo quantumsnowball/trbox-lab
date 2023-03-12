3a8670b0b2650ad54d8a70a0e9952c51d9304514
Quantum Snowball <quantum.snowball@gmail.com>
2023-03-13 00:37:37 +0800

    feat: auto switch to single row / double row layout

--- components/result/Study/PlotlyChart.tsx
+++ components/result/Study/PlotlyChart.tsx
@@ -1,9 +1,45 @@
 import { RootState } from "@/redux/store";
 import dynamic from "next/dynamic";
+import { Layout } from "plotly.js";
 import { FC } from "react";
 import { useSelector } from "react-redux";
 const Plot = dynamic(() => import('react-plotly.js'), { ssr: false });
 
+const BASE_LAYOUT: Partial<Layout> = {
+  showlegend: true,
+  legend: {
+    x: 0,
+    y: 1,
+    yanchor: 'bottom',
+    orientation: 'h',
+  },
+  margin: { l: 20, r: 50, t: 20, b: 20 },
+}
+
+const TWO_ROWS_LAYOUT: Partial<Layout> = {
+  ...BASE_LAYOUT, ...{
+    yaxis: {
+      // from bottom to top
+      domain: [0.2, 1.0],
+      side: 'right'
+    },
+    yaxis2: {
+      // from bottom to top
+      domain: [0.0, 0.2],
+      side: 'right'
+    },
+    grid: {
+      rows: 2,
+      columns: 1,
+      // subplots: ['xy1', 'xy2',],
+      // roworder: 'top to bottom',
+    }
+  }
+}
+
+const BASE_CONFIG = {
+  responsive: true,
+}
 
 const PlotlyChart: FC<{ path: string, strategy: string }> = ({ path, strategy }) => {
   const mainSeries = useSelector((s: RootState) => s.content.result.study.series[path]?.[strategy]?.main ?? {})
@@ -11,40 +47,12 @@ const PlotlyChart: FC<{ path: string, strategy: string }> = ({ path, strategy })
   const mainData = Object.entries(mainSeries).map(([_, d]) => d)
   const subData = Object.entries(subSeries).map(([_, d]) => d)
   const data = [...mainData, ...subData]
-  console.log({ data })
 
   return (
     <Plot
       data={structuredClone(data)}
-      layout={{
-        showlegend: true,
-        legend: {
-          x: 0,
-          y: 1,
-          yanchor: 'bottom',
-          orientation: 'h',
-        },
-        yaxis: {
-          // from bottom to top
-          domain: [0.2, 1.0],
-          side: 'right'
-        },
-        yaxis2: {
-          // from bottom to top
-          domain: [0.0, 0.2],
-          side: 'right'
-        },
-        margin: { l: 20, r: 50, t: 20, b: 20 },
-        grid: {
-          rows: 2,
-          columns: 1,
-          // subplots: ['xy1', 'xy2',],
-          // roworder: 'top to bottom',
-        }
-      }}
-      config={{
-        responsive: true,
-      }}
+      layout={subData.length > 0 ? TWO_ROWS_LAYOUT : BASE_LAYOUT}
+      config={BASE_CONFIG}
     />
   )
 }
