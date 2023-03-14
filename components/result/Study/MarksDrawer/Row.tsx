c6b1c792a329f4f87e7a218b9a4be706a697136e
Quantum Snowball <quantum.snowball@gmail.com>
2023-03-14 11:41:02 +0800

    rft: removeSeries

--- components/result/Study/MarksDrawer/Row.tsx
+++ components/result/Study/MarksDrawer/Row.tsx
@@ -6,7 +6,7 @@ import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined
 import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
 import { useDispatch } from "react-redux";
-import { contentActions, StudyPlotMode } from "@/redux/slices/content";
+import { contentActions, PlotTarget, StudyPlotMode } from "@/redux/slices/content";
 import { Data } from "plotly.js";
 import ControlButtons from "./ControlButtons";
 
@@ -21,19 +21,13 @@ const Row: FC<{ path: string, strategy: string, name: string }> = ({ path, strat
   const addSub1Series = (data: Data) => dispatch(contentActions.addPlotlyChartSeries({ path, strategy, name, target: 'sub1', data }))
   const addSub2Series = (data: Data) => dispatch(contentActions.addPlotlyChartSeries({ path, strategy, name, target: 'sub2', data }))
   const addOverlaySeries = (data: Data) => dispatch(contentActions.addPlotlyChartSeries({ path, strategy, name, target: 'overlay', data }))
-  const removeMainSeries = () => dispatch(contentActions.removePlotlyChartSeries({ path, strategy, name, target: 'main' }))
-  const removeSub1Series = () => dispatch(contentActions.removePlotlyChartSeries({ path, strategy, name, target: 'sub1' }))
-  const removeSub2Series = () => dispatch(contentActions.removePlotlyChartSeries({ path, strategy, name, target: 'sub2' }))
-  const removeOverlaySeries = () => dispatch(contentActions.removePlotlyChartSeries({ path, strategy, name, target: 'overlay' }))
+  const removeSeries = (target: PlotTarget) => () =>
+    dispatch(contentActions.removePlotlyChartSeries({ path, strategy, name, target }))
 
   useEffect(() => {
     (async () => {
       if (studyMode === null) {
-        removeMainSeries()
-        removeSub1Series()
-        removeSub2Series()
-        removeOverlaySeries()
-        return
+        (['main', 'sub1', 'sub2', 'overlay'] as PlotTarget[]).forEach(m => removeSeries(m)())
       }
       else if (studyMode === 'overlay') {
         const interp = firstMainSeriesName
@@ -45,10 +39,8 @@ const Row: FC<{ path: string, strategy: string, name: string }> = ({ path, strat
             const type = 'scatter'
             const mode = 'markers'
             const xaxis = 'x'
-            addOverlaySeries({ name, x, y, type, mode, xaxis, yaxis: 'y1', marker: { size: 10 } })
-            removeMainSeries()
-            removeSub1Series()
-            removeSub2Series()
+            addOverlaySeries({ name, x, y, type, mode, xaxis, yaxis: 'y1', marker: { size: 10 } });
+            (['main', 'sub1', 'sub2'] as PlotTarget[]).forEach(m => removeSeries(m)())
           }
         }
       }
@@ -61,22 +53,16 @@ const Row: FC<{ path: string, strategy: string, name: string }> = ({ path, strat
           const mode = 'lines'
           const xaxis = 'x'
           if (studyMode === 'main') {
-            addMainSeries({ name, x, y, type, mode, xaxis, yaxis: 'y1' })
-            removeSub1Series()
-            removeSub2Series()
-            removeOverlaySeries()
+            addMainSeries({ name, x, y, type, mode, xaxis, yaxis: 'y1' });
+            (['sub1', 'sub2', 'overlay'] as PlotTarget[]).forEach(m => removeSeries(m)())
           }
           else if (studyMode === 'sub1') {
-            addSub1Series({ name, x, y, type, mode, xaxis, yaxis: 'y2' })
-            removeMainSeries()
-            removeSub2Series()
-            removeOverlaySeries()
+            addSub1Series({ name, x, y, type, mode, xaxis, yaxis: 'y2' });
+            (['main', 'sub2', 'overlay'] as PlotTarget[]).forEach(m => removeSeries(m)())
           }
           else if (studyMode === 'sub2') {
-            addSub2Series({ name, x, y, type, mode, xaxis, yaxis: 'y3' })
-            removeMainSeries()
-            removeSub1Series()
-            removeOverlaySeries()
+            addSub2Series({ name, x, y, type, mode, xaxis, yaxis: 'y3' });
+            (['main', 'sub1', 'overlay'] as PlotTarget[]).forEach(m => removeSeries(m)())
           }
         }
       }
